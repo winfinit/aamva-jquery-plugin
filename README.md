582c654e04add30db5deca7e73dbf599b690512e
Roman Jurkov <winfinit@gmail.com>
2014-10-23 06:17:47 -0400

    initial release

---
+++ README.md
@@ -0,0 +1,72 @@
+# aamva-plugin.js
+
+small jquery plugin that helps to parse magnetic stripe from aamva
+
+
+## Usage
+
+```javascript
+  var stripe_data = '%FLDELRAY BEACH^DOE$JOHN$^4818 S FEDERAL BLVD^           \?\
+  ;6360100462172082009=2101198299090=?\
+  #! 33435      I               1600                                   ECCECC00000?';
+
+  var res = $.aamva.parse($stripe_data);
+
+  console.log("DMV ID:",res.id());
+  console.log("First name:",res.name().first);
+  console.log("Last name:",res.name().last);
+  console.log("Middle name:",res.name().middle);
+  console.log("Sex:",res.sex()); /* MALE, FEMALE, MISSING/INVALID */
+  console.log("Entire object", res);
+
+/* 
+  output: 
+        { state: 'FL',
+          city: 'DELRAY BEACH',
+          name: [Function],
+          address: '4818 S FEDERAL BLVD',
+          iso_iin: '636010',
+          dl: '0462172082009',
+          expiration_date: '2101',
+          birthday: '19829909',
+          dl_overflow: '0',
+          cds_version: '#',
+          jurisdiction_version: '!',
+          postal_code: '33435      ',
+          class: 'I ',
+          restrictions: '          ',
+          endorsments: '    ',
+          sex: [Function],
+          height: '600',
+          weight: '   ',
+          hair_color: '   ',
+          eye_color: '   ',
+          misc: '                          ECCECC00000',
+          id: [Function] }
+*/
+
+```
+
+## Contributing
+
+If you find a bug or willing to add some enhancement, pull requests are very welcome
+
+## Release History
+
+* 0.0.1 Initial release
+
+## Legal
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
---
+++ aamva-plugin.js
@@ -0,0 +1,61 @@
+(function($) {
+    $.aamva = {
+        stripe: function (data) {
+            data = data.replace(/\n/, "");
+            // replace spaces with regular space
+            data = data.replace(/\s/g, " ");
+            var track = data.match(/(.*?\?)(.*?\?)(.*?\?)/);
+            var res1 = track[1].match(/(\%)([A-Z]{2})([^\^]{0,13})\^?([^\^]{0,35})\^?([^\^]{0,29})\^?\s*?\?/);
+            var res2 = track[2].match(/(;)(\d{6})(\d{0,13})(\=)(\d{4})(\d{8})(\d{0,5})\=?\?/);
+            var res3 = track[3].match(/(\#|\%)(\d|\!)(\d|\s)([0-9A-Z ]{11})([0-9A-Z ]{2})([0-9A-Z ]{10})([0-9A-Z ]{4})([12]{1})([0-9A-Z ]{3})([0-9A-Z ]{3})([0-9A-Z ]{3})([0-9A-Z ]{3})(.*?)\?/);
+
+            return {
+                "state": res1[2],
+                "city": res1[3],
+                "name": res1[4],
+                "name": function() {
+                    var res = res1[4].match(/([^\$]{0,35})\$?([^\$]{0,35})?\$?([^\$]{0,35})?/);
+                    return {
+                        last: res[1],
+                        first: res[2],
+                        middle: res[3]
+                    }
+                },
+                "address": res1[5],
+                "iso_iin": res2[2],
+                "dl": res2[3],
+                "expiration_date": res2[5],
+                "birthday": res2[6],
+                "dl_overflow": res2[7],
+                "cds_version": res3[1],
+                "jurisdiction_version": res3[2],
+                "postal_code": res3[4],
+                "class": res3[5],
+                "restrictions": res3[6],
+                "endorsments": res3[7],
+                "sex": function() {
+                    switch (Number(res3[8])) {
+                        case 1:
+                            return "MALE";
+                            break;
+                        case 2:
+                            return "FAMALE";
+                            break;
+                        default:
+                            return "MISSING/INVALID";
+                            break;
+                    }
+                },
+                "height": res3[9],
+                "weight": res3[10],
+                "hair_color": res3[11],
+                "eye_color": res3[12],
+                "misc": res3[13],
+                "id": function() {
+                    var res = res2[3].match(/(\d{2})(.*)/);
+                    return (String.fromCharCode(Number(res[1]) + 64) + res[2] + res2[7]);
+                }
+            };
+        }
+    }
+})(jQuery);
